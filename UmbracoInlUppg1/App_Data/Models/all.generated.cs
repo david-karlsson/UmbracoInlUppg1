bac37ad74d7ae09f1e6a6a7e5b13456f854557fe
David Karlsson <david.karlsson1@learnet.se>
2020-10-05 11:24:25 +0200

    Page access restrictions + auto group-add for newly registered members

--- UmbracoInlUppg1/App_Data/Models/all.generated.cs
+++ UmbracoInlUppg1/App_Data/Models/all.generated.cs
@@ -7,7 +7,7 @@ using  Umbraco.Core.Models.PublishedContent;
 using  Umbraco.Web;
 using  Umbraco.ModelsBuilder.Embedded;
 [assembly:ModelsBuilderAssembly(PureLive = true, SourceHash = "9cb242a08c5b7852")]
-[assembly:System.Reflection.AssemblyVersion("0.0.0.2")]
+[assembly:System.Reflection.AssemblyVersion("0.0.0.1")]
 
 
 // FILE: models.generated.cs
